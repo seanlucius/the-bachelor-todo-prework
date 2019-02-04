b197a3b160b2843a4f7d59ac3a47bf0517359088
Sean Lucius <seanlucius22@icloud.com>
2019-02-04 21:11:37 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -22,7 +22,7 @@ def get_contestant_name(data, occupation)
 end
 
 def count_contestants_by_hometown(data, hometown)
- 
+  i = 0
   data.each do |seasons, season_arrays|
     season_arrays.each do |contestants|
       contestants.each do |key, value|
