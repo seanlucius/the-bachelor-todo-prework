747faa75674bea82860a384911f08cd45ca4ff52
Sean Lucius <seanlucius22@icloud.com>
2019-02-04 21:28:57 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -52,7 +52,7 @@ def get_average_age_for_season(data, season)
   data[season].each do |season_collection|
     season_collection.each do |key, value|
       if key == "age"
-        ages += value
+        ages += value.to_f
       end 
     end 
   end 
